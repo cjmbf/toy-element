9425ae52f3ab7f3ce3dce973cf630a3ef71a5f37
cjmbf <792233614@qq.com>
2025-01-21 11:14:07 +0800

    init

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineOptions({
+  name: "ErButton",
+});
+</script>
+
+<template>
+  <div>
+    <button style="background-color: blue; color: red">this is a button</button>
+  </div>
+</template>
